bdb32423395612fdaaf6f228d3deedba92d7cafb
Mahito TANNO <mahito@presche.me>
2024-10-22 12:45:47 +0900

    build: add 'base' to vite.config.js
    see https://vite.dev/guide/static-deploy.html for details

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
 
+const REPO_NAME = "elm-todo-app";
+
 export default defineConfig({
   plugins: [elmPlugin()],
+  base: `/${REPO_NAME}/`,
 });
